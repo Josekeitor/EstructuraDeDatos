f5f2d97088be3d8412029d0c5da5c8e40d7bd8bf
JC <jacosta291@gmail.com>
2019-08-23 14:34:23 -0500

    Version completa

--- Estructura de Datos/src/estructura/de/datos/Main.java
+++ Estructura de Datos/src/estructura/de/datos/Main.java
@@ -10,9 +10,8 @@ package estructura.de.datos;
  * @author JC
  */
 
-import estructura.de.datos.shapes.Circle;
-import estructura.de.datos.shapes.Shape;
-import estructura.de.datos.shapes.Square;
+import estructura.de.datos.shapes.*;
+
 import java.util.Scanner;
 
 public class Main {
@@ -39,6 +38,50 @@ public class Main {
                 System.out.println("Dame el radio del círculo: ");
                 double radius = in.nextDouble();
                 shape = new Circle(radius);
+                break;
+            case "3":
+                System.out.println("Dame la base del rectángulo: ");
+                double base = in.nextDouble();
+                System.out.println("Dame la altura del rectángulo: ");
+                double height = in.nextDouble();
+                
+                shape = new Rectangle(base, height);
+                break;
+            case "4":
+                System.out.println("¿Qué tipo de triángulo quieres calcular?");
+                System.out.println(" ");
+                System.out.println("1) Isóceles  2) Equilátero  3) Escaleno");
+                String triangleType = in.nextLine();
+               
+                switch(triangleType){
+                    case("1"):
+                        System.out.println("Dame la base del triángulo:");
+                        double baseIso = in.nextDouble();
+                        System.out.println("Dame 1 de los otros lados del triángulo: ");
+                        double sideIso = in.nextDouble();
+                        shape = new Isoceles(baseIso, sideIso);
+                        break;
+                    case("2"):
+                        System.out.println("Dame un lado del triángulo: ");
+                        double sideEqui = in.nextDouble();
+                        shape = new Equilateral(sideEqui);
+                        break;
+                    case("3"):
+                        System.out.println("Dame los 3 lados del triángulo separados por un espacio: ");
+                        
+                        String[] sides = in.nextLine().split(" ");
+                        shape = new Scalene(Double.parseDouble(sides[0]), Double.parseDouble(sides[1]), Double.parseDouble(sides[2]));
+                        break;
+                    default:
+                        System.out.println("Dame la base del triángulo:");
+                        double baseIsoDefault = in.nextDouble();
+                        System.out.println("Dame 1 de los otros lados del triángulo: ");
+                        double sideIsoDefault = in.nextDouble();
+                        shape = new Isoceles(baseIsoDefault, sideIsoDefault);
+                        break;
+                    
+                }
+                break;
             default:
                 System.out.println("Dame el lado del cuadrado: ");
                 double side1 = in.nextDouble();
@@ -72,7 +115,8 @@ public class Main {
     }
     
     public static String askShape(Scanner in){
-        System.out.println("Elige la figura: 1) Cuadradado   2) Círculo");
+        System.out.println("Elige la figura: 1) Cuadradado   2) Círculo  3) Rectángulo 4) Triángulo  o bien 0) D"
+                + "etener el programa");
         String option = in.nextLine();
         return option;
     }
